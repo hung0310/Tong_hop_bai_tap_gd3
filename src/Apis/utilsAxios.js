d7d13bcccb8a68a262d71a153898fb20e0243c39
hung0310 <107135919+hung0310@users.noreply.github.com>
2024-07-28 15:18:29 +0700

    bt57

--- src/Apis/utilsAxios.js
+++ src/Apis/utilsAxios.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 const base_url = 'https://qt.levanlau.net/api';
 
 export const GetAll_WithoutToken = async () => {
-    const rsp = await axios.get(`${base_url}/blog/blog_category_level_1_get_all_not_auth_api`);
-    return rsp.data;
+    try {
+        const rsp = await axios.get(`${base_url}/blog/blog_category_level_1_get_all_not_auth_api`);
+        return rsp.data;
+    } catch (error) {
+        console.error('Error:', error.message || error.response);
+        throw error;
+    }
 };
 
 export const GetAll_WithToken = async (accessToken) => {
